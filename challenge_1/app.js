1f679d2ce95c99accb10949d6dd8890e1880b9c6
Nicole <nicoledbanta@gmail.com>
2020-03-02 15:06:42 -0800

    Update 3x3 grid with each move

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,6 +5,7 @@ const row = document.getElementsByClassName('row');
 const cell = document.getElementsByClassName('cell');
 
 let results = [];
+let moves = 0;
 
 let playerTurn = ['x'];
 
@@ -17,7 +18,6 @@ for (var i = 0; i < cell.length; i++) {
     } else if (playerTurn[playerTurn.length - 1] === 'o') {
       playerTurn.push('x');
     }
-    console.log(cell[i].innerHTML);
   });
 }
 
@@ -29,16 +29,43 @@ let changeText = function (id) {
 }
 
 // Return values into an array of arrays (3x3)
-// console.log(row[0].childNodes[5].innerHTML);
+// console.log(row[0].childNodes[5]);
 for (var j = 0; j < row.length; j++) {
   var eachRow = [];
   for (var k = 1; k < 6; k += 1) {
     if (k % 2 === 0) {
       continue;
     }
-    eachRow.push(row[j].childNodes[k].innerHTML);
+    row[j].childNodes[k].addEventListener('click', function (event) {
+      moves++;
+      console.log('moves: ', moves)
+      if (moves > 2) {
+        transformToGrid();
+        console.log('check for winner');
+      };
+    });
+    // eachRow.push(row[j].childNodes[k].innerText); // DOES NOT WORK
+    // console.log('what is this? ', row[j].childNodes[k]);
+    // Leaving this to work on checkForWinner
+    // WAS Hoping to circle back to transform grid into an 3x3 array of arrays
   }
-  results.push(eachRow);
+  // results.push(eachRow); // DOES NOT WORK
 }
 
-console.log(results);
+// console.log(results);
+
+// create a helper function that maps each innerHTML of the grid into array of arrays
+let transformToGrid = function () {
+  for (var j = 0; j < row.length; j++) {
+    var eachRow = [];
+    for (var k = 1; k < 6; k += 1) {
+      if (k % 2 === 0) {
+        continue;
+      }
+      eachRow.push(row[j].childNodes[k].innerHTML);
+    }
+    results.push(eachRow);
+  }
+  console.log(results);
+}
+// create a helper function that checks for winner and invokes helper function above
