fc6c607615ebc0bce556629558ee7a2a2866bc2c
Nicole <nicoledbanta@gmail.com>
2020-03-02 12:00:23 -0800

    Click through each cell with 'x'

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,5 @@
 // identifying values so we can manipulate through dom
+// document.body.innerHTML = "";
 
 var grid = document.getElementsByClassName('grid');
 var row = document.getElementsByClassName('row');
@@ -6,23 +7,31 @@ var cell = document.getElementsByClassName('cell');
 
 var results = [];
 
-console.log(grid);
-console.log(row[0]);
-console.log(cell[0].innerHTML);
-
 // loop through array to 
 // 1. Retrieve all array values
 // 2. Bind event listeners using DOM
 
+// the below binds a div click into a turn by changing innerHTML into a letter
+// document.getElementById('turn').onclick = function changeContent() {
+//   document.getElementById('turn').innerHTML = "x";
+// }
+
+// document.getElementById('turnAgain').onclick = function changeContent() {
+//   document.getElementById('turnAgain').innerHTML = "o";
+// }
+
+// next step is to create 
+
+// be able to ask for grid values and push into an array, periodically
 for (var i = 0; i < cell.length; i++) {
   results.push(cell[i].innerHTML);
   cell[i].addEventListener('click', function (event) {
-    console.log(event.type + ' got fired')
+    console.log(event.type + ' got fired');
   });
 }
 
-cell[0].addEventListener('click', function (event) {
-  console.log(event.type + ' got fired')
-});
+function changeText(id) {
+  id.innerHTML = "X";
+}
 
 console.log(results);
