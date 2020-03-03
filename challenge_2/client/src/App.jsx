d80c495bfd616ad3cea59c1ccb09dc6cd30add60
Nicole <nicoledbanta@gmail.com>
2020-03-03 13:14:38 -0800

    Revert to HTML

--- challenge_2/client/src/App.jsx
+++ challenge_2/client/src/App.jsx
@@ -3,17 +3,31 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      jsondata: {}
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('Wait for JSON Data to Convert');
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>Convert your JSON to CSV:
-        <form>
-          <input type="text" name="jsondata" required size="20" />
-          <button>Convert</button>
+        <form action="/rhino" method="post">
+          {/* <input type="text" name="jsondata" required size="20" /> */}
+          <textarea name="jsondata"></textarea>
+          <input type="submit" />
         </form>
       </div>
     );
+  }
+
+  convertFile() {
+
   }
 }
 
--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8000;
 const path = require('path');
+const bodyParser = require('body-parser');
+const indexFile = '/Users/nicolediannep.banta/Downloads/hrsf126-mini-apps-1/challenge_2/client/dist/index.html';
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.sendFile(indexFile));
+app.post('/rhino', (req, res) => {
+  console.log('hello', req.body);
+  res.redirect('/');
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
