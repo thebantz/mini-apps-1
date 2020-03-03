10ffd72625231dec71ff67b1b16011d49c287e3c
Nicole <nicoledbanta@gmail.com>
2020-03-02 16:13:20 -0800

    Check Horizontal Winners

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,10 +9,8 @@ let moves = 0;
 
 let playerTurn = ['x'];
 
-// Below tracks who plays next and alternates 'x' and 'o' inputs
 for (var i = 0; i < cell.length; i++) {
   cell[i].addEventListener('click', function (event) {
-    // console.log(event.type + ' has been fired');
     if (playerTurn[playerTurn.length - 1] === 'x') {
       playerTurn.push('o');
     } else if (playerTurn[playerTurn.length - 1] === 'o') {
@@ -22,14 +20,11 @@ for (var i = 0; i < cell.length; i++) {
 }
 
 let changeText = function (id) {
-  // added the no-overwrite policy for each cell
   if (!id.innerHTML) {
     id.innerHTML = playerTurn[playerTurn.length - 1];
   }
 }
 
-// Return values into an array of arrays (3x3)
-// console.log(row[0].childNodes[5]);
 for (var j = 0; j < row.length; j++) {
   var eachRow = [];
   for (var k = 1; k < 6; k += 1) {
@@ -61,26 +56,35 @@ let transformToGrid = function () {
   let grid = results.slice(results.length - 3);
   return grid;
 }
+
 // create a helper function that checks for winner and invokes helper function above
 let checkForWinner = function () {
   let grid = transformToGrid();
+  let Xwins = move => move === 'x';
+  let Owins = move => move === 'o';
   console.log('from checkForWinner: ', transformToGrid());
   // Declare winning combos - Laterals (Horizontal)
-  // check one case ("x", "x", "x") @ first tile row
-  // for (var i = 0; i < grid[0].length; i++) {
-  //   let lateralWinX = move => move = 'x';
-  //   if (grid[0][i] === "x") {
+  for (var i = 0; i < grid.length; i++) {
+    let horizontalWinX = move => move === 'x';
+    let horizontalWinO = move => move === 'o';
+    if (grid[i].every(horizontalWinX)) {
+      console.log('x wins!');
+    } else if (grid[i].every(horizontalWinO)) {
+      console.log('o wins!');
+    }
+  }
 
-  //     console.log('x won the round!');
-  //   }
-  // }
-
-  let firstRowArr = grid[0];
-  let lateralWinX = move => move === 'x';
-  firstRowArr.every(lateralWinX);
-  console.log('grid[0]: ', firstRowArr);
-  console.log('does the first row win? ', firstRowArr.every(lateralWinX));
-
+  // firstRowArr.every(lateralWinX);
+  // console.log('grid[0]: ', firstRowArr);
+  // console.log('does the first row win? ', firstRowArr.every(lateralWinX));
 
   // Declare winning combos - Laterals (Vertical)
-}
+  for (var j = 0; j < grid.length; j++) {
+    if (grid[j][0] === 'x') {
+      console.log('x might win');
+    }
+  }
+
+  ///////////////// SCRATCH
+
+};
