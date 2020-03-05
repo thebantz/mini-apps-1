f7772f002eccaa5ef967302ecb1253b56c77287a
Nicole <nicoledbanta@gmail.com>
2020-03-04 20:02:29 -0800

    Add Form One

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,9 +2,52 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello from App!
+        <h3>Enter Checkout Details:</h3>
+        <FormOne />
       </div>
     )
+  }
+}
+
+
+class FormOne extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input name="name" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          E-mail:
+          <input name="email" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Password:
+          <input name="password" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Next" />
+      </form>
+    );
   }
 }
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/', express.static(path.join(__dirname, './public')));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/user', function (req, res) {
+  res.send('Got a POST request from a user');
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
